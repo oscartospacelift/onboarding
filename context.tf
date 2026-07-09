f4ef7ff2a43ad9e43ffc807bbbefc50b7ed8ccf8
Oscar Torandell <oscarto@spacelift.io>
2026-07-09 13:22:00 +0200

    Whatever

--- context.tf
+++ context.tf
@@ -32,12 +32,6 @@ resource "random_password" "context-password" {
   special = true
 }
 
-resource "random_password" "context-password-whatever" {
-  length  = 32
-  special = true
-}
-
-
 
 # This is a secret environment variable. Note how we didn't set the write_only
 # bit at all here. This setting always defaults to "true" to protect you against
